e6351dee8925c4b7232306cf0c33f1a88e006480
Sosthène Guédon <sosthene@nitrokey.com>
2022-07-22 16:53:13 +0200

    Fix historical bytes encoding

--- src/command/data.rs
+++ src/command/data.rs
@@ -1,6 +1,7 @@
 // Copyright (C) 2022 Nitrokey GmbH
 // SPDX-License-Identifier: LGPL-3.0-only
 
+use hex_literal::hex;
 use iso7816::Status;
 
 use crate::{
@@ -365,7 +366,7 @@ impl From<PasswordStatus> for [u8; 7] {
 // § 6
 // https://cardwerk.com/smart-card-standard-iso7816-4-section-8-historical-bytes/
 // TODO: Copied from Nitrokey Pro -- check for NK3
-const HISTORICAL_BYTES: &[u8] = b"0031F573C00160009000";
+const HISTORICAL_BYTES: &[u8] = &hex!("0031F573C00160009000");
 // From [apdu_dispatch](https://github.com/solokeys/apdu-dispatch/blob/644336c38beb8896ce99a0fda23551bd65bb8126/src/lib.rs)
 const EXTENDED_LENGTH_INFO: &[u8] = &[0x02, 0x02, 0x1D, 0xB9, 0x02, 0x02, 0x1D, 0xB9];
 // § 4.1.3.2 We have a button and a LED
