e18e0cb785c6e774f36b0c4acadf2245485ff26c
Sosthène Guédon <sosthene@nitrokey.com>
2022-08-04 16:14:04 +0200

    Add EcDSA test

--- tests/crypto-sequoia.rs
+++ tests/crypto-sequoia.rs
@@ -22,6 +22,28 @@ fn sign() {
         open.verify_admin(b"12345678").unwrap();
         let mut admin = open.admin_card().unwrap();
         let (material, gendate) = admin
+            .generate_key_simple(KeyType::Signing, Some(AlgoSimple::NIST256))
+            .unwrap();
+        let pubk =
+            public_key_material_to_key(&material, KeyType::Signing, &gendate, None, None).unwrap();
+
+        open.verify_user_for_signing(b"123456").unwrap();
+        let mut sign_card = open.signing_card().unwrap();
+        let mut signer = sign_card.signer_from_public(pubk.clone(), &|| {});
+        let data = [1; 32];
+        let signature = signer.sign(HashAlgorithm::SHA256, &data).unwrap();
+        assert!(pubk
+            .verify(&signature, HashAlgorithm::SHA256, &data)
+            .is_ok());
+    });
+
+    vpicc::with_vsc(|| {
+        let mut cards = PcscBackend::cards(None).unwrap();
+        let mut pgp = OpenPgp::new(&mut cards[0]);
+        let mut open = Open::new(pgp.transaction().unwrap()).unwrap();
+        open.verify_admin(b"12345678").unwrap();
+        let mut admin = open.admin_card().unwrap();
+        let (material, gendate) = admin
             .generate_key_simple(KeyType::Signing, Some(AlgoSimple::Curve25519))
             .unwrap();
         let pubk =
