8ab346ffb3abc1b52b42679e36c06f0d16f6ff05
Sosthène Guédon <sosthene@nitrokey.com>
2022-10-07 17:01:09 +0200

    Add RSA key generation

--- src/command/gen.rs
+++ src/command/gen.rs
@@ -3,7 +3,7 @@
 
 use hex_literal::hex;
 use iso7816::Status;
-use trussed::types::{KeyId, KeySerialization, Location, StorageAttributes};
+use trussed::types::{KeyId, KeySerialization, Location, Mechanism, StorageAttributes};
 use trussed::{syscall, try_syscall};
 
 use crate::card::LoadedContext;
@@ -34,6 +34,7 @@ pub fn sign<const R: usize, T: trussed::Client>(
         SignatureAlgorithm::EcDsaP256 => {
             gen_ec_key(ctx.lend(), KeyType::Sign, CurveAlgo::EcDsaP256)
         }
+        SignatureAlgorithm::Rsa2k => gen_rsa_key(ctx.lend(), KeyType::Sign, Mechanism::Rsa2kPkcs),
         _ => {
             error!("Unimplemented operation");
             Err(Status::ConditionsOfUseNotSatisfied)
@@ -49,6 +50,7 @@ pub fn dec<const R: usize, T: trussed::Client>(
     match algo {
         DecryptionAlgorithm::X255 => gen_ec_key(ctx.lend(), KeyType::Dec, CurveAlgo::X255),
         DecryptionAlgorithm::EcDhP256 => gen_ec_key(ctx.lend(), KeyType::Dec, CurveAlgo::EcDhP256),
+        DecryptionAlgorithm::Rsa2k => gen_rsa_key(ctx.lend(), KeyType::Dec, Mechanism::Rsa2kPkcs),
         _ => {
             error!("Unimplemented operation");
             Err(Status::ConditionsOfUseNotSatisfied)
@@ -66,11 +68,46 @@ pub fn aut<const R: usize, T: trussed::Client>(
         AuthenticationAlgorithm::EcDsaP256 => {
             gen_ec_key(ctx.lend(), KeyType::Aut, CurveAlgo::EcDsaP256)
         }
+        AuthenticationAlgorithm::Rsa2k => {
+            gen_rsa_key(ctx.lend(), KeyType::Aut, Mechanism::Rsa2kPkcs)
+        }
         _ => {
             error!("Unimplemented operation");
             Err(Status::ConditionsOfUseNotSatisfied)
         }
     }
+}
+
+fn gen_rsa_key<const R: usize, T: trussed::Client>(
+    ctx: LoadedContext<'_, R, T>,
+    key: KeyType,
+    mechanism: Mechanism,
+) -> Result<(), Status> {
+    let client = ctx.backend.client_mut();
+    let key_id = try_syscall!(client.generate_key(
+        mechanism,
+        StorageAttributes::new().set_persistence(Location::Internal)
+    ))
+    .map_err(|_err| {
+        error!("Failed to generate key: {_err:?}");
+        Status::UnspecifiedNonpersistentExecutionError
+    })?
+    .key;
+
+    if let Some((old_key, _)) = ctx
+        .state
+        .internal
+        .set_key_id(key, Some((key_id, KeyOrigin::Generated)), client)
+        .map_err(|_| Status::UnspecifiedNonpersistentExecutionError)?
+    {
+        // Deletion is not a fatal error
+        try_syscall!(client.delete(old_key))
+            .inspect_err_stable(|_err| {
+                error!("Failed to delete old key: {_err:?}");
+            })
+            .ok();
+    }
+    read_rsa_key(ctx, key_id, mechanism)
 }
 
 fn gen_ec_key<const R: usize, T: trussed::Client>(
@@ -117,6 +154,7 @@ pub fn read_sign<const R: usize, T: trussed::Client>(
     match algo {
         SignatureAlgorithm::Ed255 => read_ec_key(ctx.lend(), key_id, CurveAlgo::Ed255),
         SignatureAlgorithm::EcDsaP256 => read_ec_key(ctx.lend(), key_id, CurveAlgo::EcDsaP256),
+        SignatureAlgorithm::Rsa2k => read_rsa_key(ctx.lend(), key_id, Mechanism::Rsa2kPkcs),
         _ => {
             error!("Unimplemented operation");
             Err(Status::ConditionsOfUseNotSatisfied)
@@ -137,6 +175,7 @@ pub fn read_dec<const R: usize, T: trussed::Client>(
     match algo {
         DecryptionAlgorithm::X255 => read_ec_key(ctx.lend(), key_id, CurveAlgo::X255),
         DecryptionAlgorithm::EcDhP256 => read_ec_key(ctx.lend(), key_id, CurveAlgo::EcDhP256),
+        DecryptionAlgorithm::Rsa2k => read_rsa_key(ctx.lend(), key_id, Mechanism::Rsa2kPkcs),
         _ => {
             error!("Unimplemented operation");
             Err(Status::ConditionsOfUseNotSatisfied)
@@ -157,6 +196,7 @@ pub fn read_aut<const R: usize, T: trussed::Client>(
     match algo {
         AuthenticationAlgorithm::Ed255 => read_ec_key(ctx.lend(), key_id, CurveAlgo::Ed255),
         AuthenticationAlgorithm::EcDsaP256 => read_ec_key(ctx.lend(), key_id, CurveAlgo::EcDsaP256),
+        AuthenticationAlgorithm::Rsa2k => read_rsa_key(ctx.lend(), key_id, Mechanism::Rsa2kPkcs),
         _ => {
             error!("Unimplemented operation");
             Err(Status::ConditionsOfUseNotSatisfied)
@@ -209,4 +249,51 @@ fn read_ec_key<const R: usize, T: trussed::Client>(
     let offset = ctx.reply.len();
     serialize_pub(curve, ctx.lend(), &serialized)?;
     ctx.reply.prepend_len(offset)
+}
+
+fn read_rsa_key<const R: usize, T: trussed::Client>(
+    mut ctx: LoadedContext<'_, R, T>,
+    key_id: KeyId,
+    mechanism: Mechanism,
+) -> Result<(), Status> {
+    let client = ctx.backend.client_mut();
+    let public_key = syscall!(client.derive_key(
+        mechanism,
+        key_id,
+        None,
+        StorageAttributes::new().set_persistence(Location::Volatile)
+    ))
+    .key;
+    ctx.reply.expand(KEYGEN_DO_TAG)?;
+    let offset = ctx.reply.len();
+
+    let serialized_n =
+        try_syscall!(client.serialize_key(mechanism, public_key, KeySerialization::RsaN))
+            .map_err(|_err| {
+                error!("Failed to serialize public key N: {_err:?}");
+                syscall!(client.delete(public_key));
+                Status::UnspecifiedNonpersistentExecutionError
+            })?
+            .serialized_key;
+    ctx.reply.expand(&[0x81])?;
+    ctx.reply.append_len(serialized_n.len())?;
+    ctx.reply.expand(&serialized_n)?;
+    drop(serialized_n);
+
+    let serialized_e =
+        try_syscall!(client.serialize_key(mechanism, public_key, KeySerialization::RsaE))
+            .map_err(|_err| {
+                error!("Failed to serialize public key E: {_err:?}");
+                syscall!(client.delete(public_key));
+                Status::UnspecifiedNonpersistentExecutionError
+            })?
+            .serialized_key;
+    ctx.reply.expand(&[0x82])?;
+    ctx.reply.append_len(serialized_e.len())?;
+    ctx.reply.expand(&serialized_e)?;
+
+    ctx.reply.prepend_len(offset)?;
+
+    syscall!(client.delete(public_key));
+    Ok(())
 }
